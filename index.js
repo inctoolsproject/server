ae9c166006ea6218d5559c528541b308fa6f5b06
inctoolsproject <31398833+inctoolsproject@users.noreply.github.com>
2025-05-05 09:26:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // ganti dengan domain frontend kamu
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
